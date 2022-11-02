01156d6afaf12e87f2c92ada9b49c69397dda1cf
Sariah Campopiano <scampopiano925@gmail.com>
2022-11-01 20:07:51 -0500

    Created classes for subscriptions, reactions, post_reactions, categories

---
+++ models/categories.py
@@ -0,0 +1,4 @@
+class Subscriptions():
+    def __init__(self, id, label):
+        self.id = id
+        self.label = label
---
+++ models/post_reactions.py
@@ -0,0 +1,6 @@
+class Subscriptions():
+    def __init__(self, id, reaction_id, user_id, post_id):
+        self.id = id
+        self.reaction_id = reaction_id
+        self.user_id = user_id
+        self.post_id = post_id
