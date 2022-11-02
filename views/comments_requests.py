4a4cb72633f0f7402fe49917b75f71bf58c8c44c
Phantom-Farmer <joeljdahl@gmail.com>
2022-11-01 20:54:27 -0500

    started comments

---
+++ views/comments_requests.py
@@ -0,0 +1,34 @@
+import sqlite3
+import json
+from models import comments
+
+COMMENTS = [
+  {
+            "id": 1,
+            "author_id": "",
+            "post_id": "",
+            "content": ""
+        },
+        {
+            "id": 2,
+            "author_id": "",
+            "post_id": "",
+            "content": ""
+        }
+]
+
+def post_comment():
+    """_summary_
+    """
+with sqlite3.connect('./db.sqlite3') as conn:
+    conn.row_factory = sqlite3.Row
+    db_cursor = conn.cursor()
+    
+    db_cursor.execute("""
+        SELECT
+            c.id,
+            c.author_id,
+            c.post_id,
+            c.content
+        FROM Comment c
+        """)
