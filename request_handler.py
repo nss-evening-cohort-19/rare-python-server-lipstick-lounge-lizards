ad22c791b342241e8a3d66eaa8c6e874fdcc1081
Kristen Chandler <krismchandler@gmail.com>
2022-11-02 20:22:19 -0500

    created function to create new post and tested sucessfully in postman. also fixed an error found in the do_POST when testing

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from views import create_user, login_user, get_all_posts, get_single_post
+from views import create_user, login_user, get_all_posts, get_single_post, create_post
 
 
 class HandleRequests(BaseHTTPRequestHandler):
@@ -91,7 +91,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         content_len = int(self.headers.get('content-length', 0))
         post_body = json.loads(self.rfile.read(content_len))
         response = ''
-        resource, _ = self.parse_url()
+        resource, _ = self.parse_url(self.path)
 
         if resource == 'login':
             response = login_user(post_body)
--- views/posts_requests.py
+++ views/posts_requests.py
@@ -60,3 +60,23 @@ def get_single_post(id):
                     data['publication_date'], data['image_url'], data['content'], data['approved'])
 
         return json.dumps(post.__dict__)
+
+def create_post(new_post):
+    '''creates a new entry to the post table'''
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO Posts
+            ( id, user_id, category_id, title, publication_date, image_url, content, approved )
+        VALUES
+            ( ?, ?, ?, ?, ?, ?, ?, ?);
+        """, (new_post['id'], new_post['user_id'],
+              new_post['category_id'], new_post['title'],
+              new_post['publication_date'], new_post['image_url'], new_post['content'], new_post['approved']))
+
+        id = db_cursor.lastrowid
+
+        new_post['id'] = id
+
+    return json.dumps(new_post)
