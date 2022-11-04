e624f37b425309b769a65656ab6fd480aa3db9d9
Phantom-Farmer <joeljdahl@gmail.com>
2022-11-03 19:43:38 -0500

    k merge 1

--- views/comments_requests.py
+++ views/comments_requests.py
@@ -23,7 +23,7 @@ def post_comment():
 with sqlite3.connect('./db.sqlite3') as conn:
     conn.row_factory = sqlite3.Row
     db_cursor = conn.cursor()
-    
+
     db_cursor.execute("""
         SELECT
             c.id,
