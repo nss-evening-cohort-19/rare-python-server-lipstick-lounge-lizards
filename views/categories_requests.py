cffb6643fba669d69c08e4b221545e95aa79ae7b
Sariah Campopiano <scampopiano925@gmail.com>
2022-11-05 12:46:27 -0500

    Added create_category method to categories_request

--- views/categories_requests.py
+++ views/categories_requests.py
@@ -3,6 +3,7 @@ import json
 from models import Categories
 
 def get_all_categories():
+    """Get all categories"""
     with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
@@ -24,3 +25,19 @@ def get_all_categories():
             categories.append(category.__dict__)
   
     return json.dumps(categories)
+
+def create_category(new_category):
+    """Creating a new category then adding it to 'Categories' table in db.sqlite3"""
+    with sqlite3.connect('./db.sqlite3') as conn:
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        INSERT INTO Categories
+            ( label )
+        VALUES
+            ( ? );
+        """, (new_category['label']))
+        
+        id = db_cursor.lastrowid
+        
+        new_category['id'] = id
