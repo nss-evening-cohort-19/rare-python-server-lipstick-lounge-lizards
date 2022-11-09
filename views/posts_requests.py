985c30cae41677a2097dfc68d78e8bb563f17680
Kristen Chandler <krismchandler@gmail.com>
2022-11-08 19:16:27 -0600

    merge

--- views/posts_requests.py
+++ views/posts_requests.py
@@ -117,3 +117,30 @@ def delete_post(id):
         DELETE FROM Posts
         WHERE id = ?
         """, (id, ))
+
+def get_post_by_title(title):
+    '''get a single post by title'''
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.publication_date,
+            p.image_url,
+            p.content,
+            p.approved
+        FROM Posts p
+        WHERE p.title = ?
+        """, ( title, ))
+
+        data = db_cursor.fetchone()
+
+        post = Posts(data['id'], data['user_id'], data['category_id'], data['title'],
+                    data['publication_date'], data['image_url'], data['content'], data['approved'])
+
+        return json.dumps(post.__dict__)
