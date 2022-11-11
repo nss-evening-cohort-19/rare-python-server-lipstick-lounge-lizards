3e6d5a8076fd1d837eb48cb4ce61f7ada7b0d48a
Kristen Chandler <krismchandler@gmail.com>
2022-11-10 19:20:16 -0600

    started method to update post tags

--- views/post_tags_requests.py
+++ views/post_tags_requests.py
@@ -18,3 +18,23 @@ def create_post_tags(new_post_tag):
         new_post_tag['id'] = id
 
     return json.dumps(new_post_tag)
+
+def update_post_tags(id, new_post_tag):
+    '''updates a tag on a post'''
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        Update PostTags
+            SET
+                post_id = ?,
+                tag_id =?
+        WHERE id = ?
+        """, (new_post_tag['post_id'], new_post_tag['tag_id']))
+
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        return False
+    else:
+        return True
